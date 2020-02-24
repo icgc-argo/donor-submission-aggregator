673bc20a0415bdb51303c417b4bc668d8b77f4b8
Minh <hlminh2000@gmail.com>
2020-02-24 13:56:26 -0500

    :wrench: updates env schema

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const MONGO_URL =
   process.env.MONGO_URL || "mongodb://localhost:27017/clinical";
-export const MONGO_USER = process.env.MONG_OUSER || "http://localhost:9200";
-export const MONGO_PASS = process.env.MONGO_PASS || "http://localhost:9200";
+export const MONGO_USER = process.env.MONG_OUSER;
+export const MONGO_PASS = process.env.MONGO_PASS;
 
 export const ES_HOST = process.env.ES_HOST || "http://localhost:9200";
 
