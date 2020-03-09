f8f91bd21967d2a2889260ecb08832787073e9ff
Minh Ha <hlminh2000@gmail.com>
2020-03-09 14:32:47 -0400

    ✨ vault integration for Mongo (#24)
    * ✨ initial vault integration
    
    * ✨ implements vault integration for mongo connection
    
    * ✅ adds tests to check if mongo connects without vault
    
    * 💚 removes unused test
    
    * ♻️ changes mongo secret shape
    
    * 🔥 removes unused var
    
    * 🔥 removes VAULT_K8_TOKEN_PATH
    
    * 🐛 removes unused import

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,11 @@
 import dotenv from "dotenv";
-dotenv.config();
+import path from "path";
+
+export const APP_DIR = __dirname;
+
+dotenv.config({
+  path: path.resolve(APP_DIR, "../.env")
+});
 
 export const MONGO_URL =
   process.env.MONGO_URL || "mongodb://localhost:27017/clinical";
@@ -8,10 +14,13 @@ export const MONGO_PASS = process.env.MONGO_PASS;
 
 export const ES_HOST = process.env.ES_HOST || "http://localhost:9200";
 
-export const ROLLCALL_SERVICE_ROOT = process.env.ROLLCALL_SERVICE_ROOT || "http://localhost:9001";
-export const ROLLCALL_INDEX_ENTITY = process.env.ROLLCALL_INDEX_ENTITY || "donor";
+export const ROLLCALL_SERVICE_ROOT =
+  process.env.ROLLCALL_SERVICE_ROOT || "http://localhost:9001";
+export const ROLLCALL_INDEX_ENTITY =
+  process.env.ROLLCALL_INDEX_ENTITY || "donor";
 export const ROLLCALL_INDEX_TYPE = process.env.ROLLCALL_INDEX_TYPE || "centric";
-export const ROLLCALL_INDEX_SHARDPREFIX = process.env.ROLLCALL_INDEX_SHARDPREFIX || "pgm";
+export const ROLLCALL_INDEX_SHARDPREFIX =
+  process.env.ROLLCALL_INDEX_SHARDPREFIX || "pgm";
 
 export const STREAM_CHUNK_SIZE = Number(process.env.STREAM_CHUNK_SIZE) || 1000;
 
@@ -36,3 +45,48 @@ export const PARTITIONS_CONSUMED_CONCURRENTLY = process.env
 export const PORT = Number(process.env.PORT || 7000);
 
 export const ENABLED = process.env.ENABLED === "true";
+
+/**
+ * VAULT configurations
+ */
+export const USE_VAULT = process.env.USE_VAULT === "true";
+export const VAULT_TOKEN = process.env.VAULT_TOKEN as string;
+export const VAULT_AUTH_METHOD = process.env.VAULT_AUTH_METHOD as
+  | "token"
+  | "kubernetes";
+export const VAULT_URL = process.env.VAULT_URL as string;
+export const VAULT_ROLE = process.env.VAULT_ROLE as string;
+export const VAULT_MONGO_SECRET_PATH = process.env
+  .VAULT_MONGO_SECRET_PATH as string;
+const REQUIRED_VAULT_CONFIGS = {
+  VAULT_AUTH_METHOD,
+  VAULT_URL,
+  VAULT_ROLE,
+  VAULT_MONGO_SECRET_PATH
+};
+const missingValue = ([key, value]: [string, any]) => !value;
+if (USE_VAULT && Object.entries(REQUIRED_VAULT_CONFIGS).some(missingValue)) {
+  const error = new Error(
+    `USE_VAULT is true but missing configs: ${Object.entries(
+      REQUIRED_VAULT_CONFIGS
+    )
+      .filter(missingValue)
+      .map(([key]) => key)}`
+  );
+  throw error;
+}
+const REQUIRED_K8_CONFIGS = {
+  VAULT_ROLE
+};
+if (USE_VAULT && VAULT_AUTH_METHOD === "kubernetes") {
+  if (Object.entries(REQUIRED_K8_CONFIGS).some(missingValue)) {
+    const error = new Error(
+      `VAULT_AUTH_METHOD is "kubernetes" but missing configs: ${Object.entries(
+        REQUIRED_K8_CONFIGS
+      )
+        .filter(missingValue)
+        .map(([key]) => key)}`
+    );
+    throw error;
+  }
+}
---
+++ src/vault/index.ts
@@ -0,0 +1,36 @@
+import vault, { VaultOptions } from "node-vault";
+import { VAULT_URL, VAULT_ROLE, VAULT_TOKEN, VAULT_AUTH_METHOD } from "config";
+import { promises } from "fs";
+import logger from "logger";
+
+export const createVaultClient = async (vaultOptions: VaultOptions = {}) => {
+  const options: VaultOptions = {
+    apiVersion: "v1",
+    endpoint: VAULT_URL,
+    token: VAULT_TOKEN,
+    ...vaultOptions
+  };
+  const vaultClient = vault(options);
+
+  if (VAULT_AUTH_METHOD === "kubernetes") {
+    const k8Token =
+      VAULT_TOKEN ||
+      (await promises.readFile(
+        "/var/run/secrets/kubernetes.io/serviceaccount/token",
+        "utf-8"
+      ));
+    await vaultClient.kubernetesLogin({
+      role: VAULT_ROLE,
+      jwt: k8Token
+    });
+  }
+  return vaultClient;
+};
+
+export const loadVaultSecret = (
+  vaultClient: ReturnType<typeof createVaultClient>
+) => async (path: string) => {
+  const result = await (await vaultClient).read(path);
+  logger.info(`Loaded Vault secret at ${path}`);
+  return result.data as { [k: string]: any };
+};
