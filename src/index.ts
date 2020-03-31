91e0e4ca4da53ae3db45d92e33478ac9b48f6c27
Minh <hlminh2000@gmail.com>
2020-03-31 15:21:12 -0400

    ✨ improves startup

--- src/index.ts
+++ src/index.ts
@@ -25,44 +25,47 @@ import {
 import applyStatusRepor from "./statusReport";
 import logger from "logger";
 
-/**
- * Express app to host status reports and other interface for interacting with this app
- */
-const expressApp = express();
-const statusReporter = applyStatusRepor(expressApp)("/status");
-expressApp.use(
-  "/",
-  swaggerUi.serve,
-  swaggerUi.setup(yaml.load(path.join(__dirname, "./assets/swagger.yaml")))
-);
-expressApp.listen(7000, () => {
-  logger.info(`Start readiness check at :${PORT}/status`);
-});
+(async () => {
+  /**
+   * Express app to host status reports and other interface for interacting with this app
+   */
+  const expressApp = express();
+  const statusReporter = applyStatusRepor(expressApp)("/status");
+  expressApp.use(
+    "/",
+    swaggerUi.serve,
+    swaggerUi.setup(yaml.load(path.join(__dirname, "./assets/swagger.yaml")))
+  );
+  expressApp.listen(7000, () => {
+    logger.info(`Start readiness check at :${PORT}/status`);
+  });
 
-/**
- * The main Kafka subscription
- */
-if (ENABLED) {
-  (async () => {
-    await connectMongo();
-    const esClient = await createEsClient();
+  await connectMongo();
+  const esClient = await createEsClient();
 
-    const rollCallClient = createRollcallClient({
-      url: ROLLCALL_SERVICE_ROOT,
-      entity: ROLLCALL_INDEX_ENTITY,
-      type: ROLLCALL_INDEX_TYPE,
-      shardPrefix: ROLLCALL_INDEX_SHARDPREFIX
-    });
+  const rollCallClient = createRollcallClient({
+    url: ROLLCALL_SERVICE_ROOT,
+    entity: ROLLCALL_INDEX_ENTITY,
+    type: ROLLCALL_INDEX_TYPE,
+    shardPrefix: ROLLCALL_INDEX_SHARDPREFIX
+  });
 
-    const kafka = new Kafka({
-      clientId: `donor-submission-aggregator`,
-      brokers: KAFKA_BROKERS
+  const kafka = new Kafka({
+    clientId: `donor-submission-aggregator`,
+    brokers: KAFKA_BROKERS
+  });
+  const consumer = kafka.consumer({
+    groupId: KAFKA_CONSUMER_GROUP
+  });
+  await consumer.connect();
+
+  /**
+   * The main Kafka subscription
+   */
+  if (ENABLED) {
+    await consumer.subscribe({
+      topic: CLINICAL_PROGRAM_UPDATE_TOPIC
     });
-    const consumer = kafka.consumer({
-      groupId: KAFKA_CONSUMER_GROUP
-    });
-    await consumer.connect();
-    await consumer.subscribe({ topic: CLINICAL_PROGRAM_UPDATE_TOPIC });
     await consumer.run({
       partitionsConsumedConcurrently: PARTITIONS_CONSUMED_CONCURRENTLY,
       eachMessage: async ({ message }) => {
@@ -81,6 +84,6 @@ if (ENABLED) {
         }
       }
     });
-    statusReporter.setReady(true);
-  })();
-}
+  }
+  statusReporter.setReady(true);
+})();
