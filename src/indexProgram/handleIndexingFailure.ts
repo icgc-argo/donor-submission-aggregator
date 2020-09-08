a8b03b18432336664a41ceb2b7a90b7f81bde563
Minh <hlminh2000@gmail.com>
2020-09-08 16:31:08 -0400

    puts back indexProgram

---
+++ src/indexProgram/handleIndexingFailure.ts
@@ -0,0 +1,20 @@
+import { Client } from "@elastic/elasticsearch";
+import { ResolvedIndex } from "rollCall/types";
+import logger from "logger";
+
+export const handleIndexingFailure = async ({
+  esClient,
+  rollCallIndex,
+}: {
+  esClient: Client;
+  rollCallIndex: ResolvedIndex;
+}) => {
+  await esClient.indices
+    .delete({
+      index: rollCallIndex.indexName,
+    })
+    .catch((err) => {
+      logger.warn(`could not delete index ${rollCallIndex.indexName}: ${err}`);
+    });
+  logger.warn(`index ${rollCallIndex.indexName} was removed`);
+};
