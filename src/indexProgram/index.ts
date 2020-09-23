9949ebe1572e72995bbe860d753ad41f68459d81
Wajiha Shah <wshah@oicr.on.ca>
2020-09-23 13:28:43 -0400

    add read only before rebase

--- src/indexProgram/index.ts
+++ src/indexProgram/index.ts
@@ -1,10 +1,11 @@
 import transformToEsDonor from "./transformToEsDonor";
 import programDonorStream from "./programDonorStream";
 import { toEsBulkIndexActions } from "elasticsearch";
-import { toJson } from "donorModel";
 import { STREAM_CHUNK_SIZE } from "config";
 import { Client } from "@elastic/elasticsearch";
 import logger from "logger";
+import { EsDonorDocument } from "./types";
+import esb from "elastic-builder";
 
 export default async (
   programShortName: string,
@@ -12,7 +13,7 @@ export default async (
   esClient: Client
 ) => {
   const donorStream = programDonorStream(programShortName, {
-    chunkSize: STREAM_CHUNK_SIZE
+    chunkSize: STREAM_CHUNK_SIZE,
   });
   let chunksCount = 0;
   for await (const chunk of donorStream) {
@@ -20,12 +21,46 @@ export default async (
       chunk.length
     } donor(s) from chunk #${chunksCount++} of program ${programShortName}`;
     logger.profile(timer);
-    const esDocuments = await Promise.all(
-      chunk.map(toJson).map(transformToEsDonor)
-    );
+
+    const donorIds = chunk.map((donor) => `DO${donor.donorId}`);
+
+    const esQuery = esb
+      .requestBodySearch()
+      .query(esb.termsQuery("donorId", donorIds));
+
+    const esHits: Array<any> = await esClient
+      .search({
+        // providing an index results in inablity to detect preixsting donors
+        // providing the alias results in index not found error
+        // index: aliasName,
+        body: esQuery,
+      })
+      .then((res) => res.body.hits.hits)
+      .catch((err) => {
+        logger.error(
+          "error in grabbing donors by id from Elasticsearch: ",
+          err
+        );
+        return [];
+      });
+
+    // can add a type for esHit in types
+    const preExistingDonorIds = esHits.map((hit) => hit._source.donorId);
+
+    const esDocuments: Array<EsDonorDocument> = [];
+    for await (const donor of chunk) {
+      if (preExistingDonorIds.includes(`DO${donor.donorId}`)) {
+        // keep all NON mongo doc data, combine that with the most up to date mongo doc data
+      } else {
+        esDocuments.push(await transformToEsDonor(donor));
+      }
+    }
+
+    // const esDocuments = await Promise.all(chunk.map(transformToEsDonor));
+
     await esClient.bulk({
       body: toEsBulkIndexActions(targetIndexName)(esDocuments),
-      refresh: "true"
+      refresh: "true",
     });
     logger.profile(timer);
   }
