2a7d710b1a4895fb62d5b196915847e9823c7110
Alexis Li <alexis.li@oicr.on.ca>
2020-11-16 10:54:24 -0500

    all tests should pass

--- src/programQueueProcessor/unit.test.ts
+++ src/programQueueProcessor/unit.test.ts
@@ -183,7 +183,7 @@ describe("kafka integration", () => {
   });
 
   describe("programQueueProcessor", () => {
-    it.only("must index all clinical and RDPC data into Elasticsearch", async () => {
+    it("must index all clinical and RDPC data into Elasticsearch", async () => {
       const mockAnalysisFetcher: typeof fetchAnalyses = async (
         studyId: string,
         rdpcUrl: string,
