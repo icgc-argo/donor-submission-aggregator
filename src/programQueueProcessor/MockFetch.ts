733bf5b872c0122dcee9ac25ade202ed40f98e76
Alexis Li <alexis.li@oicr.on.ca>
2021-05-20 11:35:49 -0400

    run test

---
+++ src/programQueueProcessor/MockFetch.ts
@@ -0,0 +1,164 @@
+import fetchAnalyses from "rdpc/query/fetchAnalyses";
+import fetchAnalysesWithSpecimens from "rdpc/query/fetchAnalysesWithSpecimens";
+import fetchVariantCallingAnalyses from "rdpc/query/fetchVariantCallingAnalyses";
+import {
+  seqAlignmentAnalyses_mutect,
+  seqAlignmentAnalyses_sanger,
+  seqExpAnalyses,
+  seqExpAnalysesWithSpecimens,
+  seqExpAnalysesWithSpecimens_incremental,
+  seqExpAnalysesWithSpecimens_incremental_after_unpublish,
+  seqExpAnalyses_incremental,
+  seqExpAnalyses_incremental_after_unpublish,
+  variantCallingAnalyses,
+} from "rdpc/test/fixtures/integrationTest/mockAnalyses";
+import { Analysis, AnalysisType } from "rdpc/types";
+
+export const mockVariantCallingFetcher: typeof fetchVariantCallingAnalyses = ({
+  studyId,
+  rdpcUrl,
+  from,
+  size,
+  egoJwtManager,
+  donorId,
+}): Promise<Analysis[]> => {
+  const matchesDonorId = (donor: any) =>
+    donorId ? donor.donorId === donorId : true;
+  return Promise.resolve(
+    variantCallingAnalyses
+      .filter((analysis) => analysis.donors.some(matchesDonorId))
+      .slice(from, from + size)
+  );
+};
+
+export const mockAnalysesWithSpecimensFetcher: typeof fetchAnalysesWithSpecimens = async ({
+  studyId,
+  rdpcUrl,
+  from,
+  size,
+  egoJwtManager,
+  donorId,
+}): Promise<Analysis[]> => {
+  const matchesDonorId = (donor: any) =>
+    donorId ? donor.donorId === donorId : true;
+  return Promise.resolve(
+    seqExpAnalysesWithSpecimens
+      .filter((analysis) => analysis.donors.some(matchesDonorId))
+      .slice(from, from + size)
+  );
+};
+
+export const mockAnalysesWithSpecimensFetcher_incremental: typeof fetchAnalysesWithSpecimens = async ({
+  studyId,
+  rdpcUrl,
+  from,
+  size,
+  egoJwtManager,
+  donorId,
+}): Promise<Analysis[]> => {
+  const matchesDonorId = (donor: any) =>
+    donorId ? donor.donorId === donorId : true;
+  return Promise.resolve(
+    seqExpAnalysesWithSpecimens_incremental
+      .filter((analysis) => analysis.donors.some(matchesDonorId))
+      .slice(from, from + size)
+  );
+};
+
+export const mockAnalysesWithSpecimensFetcher_incremental_after_unpublish: typeof fetchAnalysesWithSpecimens = async ({
+  studyId,
+  rdpcUrl,
+  from,
+  size,
+  egoJwtManager,
+  donorId,
+}): Promise<Analysis[]> => {
+  const matchesDonorId = (donor: any) =>
+    donorId ? donor.donorId === donorId : true;
+  return Promise.resolve(
+    seqExpAnalysesWithSpecimens_incremental_after_unpublish
+      .filter((analysis) => analysis.donors.some(matchesDonorId))
+      .slice(from, from + size)
+  );
+};
+
+export const mockAnalysisFetcher: typeof fetchAnalyses = async ({
+  studyId,
+  rdpcUrl,
+  analysisType,
+  isMutect,
+  from,
+  size,
+  egoJwtManager,
+  donorId,
+}): Promise<Analysis[]> => {
+  const matchesDonorId = (donor: any) =>
+    donorId ? donor.donorId === donorId : true;
+  return Promise.resolve(
+    analysisType === AnalysisType.SEQ_EXPERIMENT
+      ? seqExpAnalyses
+          .filter((analysis) => analysis.donors.some(matchesDonorId))
+          .slice(from, from + size)
+      : isMutect
+      ? seqAlignmentAnalyses_mutect
+          .filter((analysis) => analysis.donors.some(matchesDonorId))
+          .slice(from, from + size)
+      : seqAlignmentAnalyses_sanger
+          .filter((analysis) => analysis.donors.some(matchesDonorId))
+          .slice(from, from + size)
+  );
+};
+
+export const mockAnalysisFetcher_incremental: typeof fetchAnalyses = async ({
+  studyId,
+  rdpcUrl,
+  analysisType,
+  isMutect,
+  from,
+  size,
+  egoJwtManager,
+  donorId,
+}): Promise<Analysis[]> => {
+  const matchesDonorId = (donor: any) =>
+    donorId ? donor.donorId === donorId : true;
+  return Promise.resolve(
+    analysisType === AnalysisType.SEQ_EXPERIMENT
+      ? seqExpAnalyses_incremental
+          .filter((analysis) => analysis.donors.some(matchesDonorId))
+          .slice(from, from + size)
+      : isMutect
+      ? seqAlignmentAnalyses_mutect
+          .filter((analysis) => analysis.donors.some(matchesDonorId))
+          .slice(from, from + size)
+      : seqAlignmentAnalyses_sanger
+          .filter((analysis) => analysis.donors.some(matchesDonorId))
+          .slice(from, from + size)
+  );
+};
+
+export const mockAnalysisFetcher_incremental_after_unpublish: typeof fetchAnalyses = async ({
+  studyId,
+  rdpcUrl,
+  analysisType,
+  isMutect,
+  from,
+  size,
+  egoJwtManager,
+  donorId,
+}): Promise<Analysis[]> => {
+  const matchesDonorId = (donor: any) =>
+    donorId ? donor.donorId === donorId : true;
+  return Promise.resolve(
+    analysisType === AnalysisType.SEQ_EXPERIMENT
+      ? seqExpAnalyses_incremental_after_unpublish
+          .filter((analysis) => analysis.donors.some(matchesDonorId))
+          .slice(from, from + size)
+      : isMutect
+      ? seqAlignmentAnalyses_mutect
+          .filter((analysis) => analysis.donors.some(matchesDonorId))
+          .slice(from, from + size)
+      : seqAlignmentAnalyses_sanger
+          .filter((analysis) => analysis.donors.some(matchesDonorId))
+          .slice(from, from + size)
+  );
+};
--- src/programQueueProcessor/unit.test.ts
+++ src/programQueueProcessor/unit.test.ts
@@ -25,18 +25,10 @@ import {
   expectedRDPCData_incremental_after_unpublish,
   testDonorIds,
 } from "rdpc/test/fixtures/integrationTest/dataset";
-import fetchAnalyses from "rdpc/query/fetchAnalyses";
-import { Analysis, AnalysisType } from "rdpc/types";
 import {
-  seqAlignmentAnalyses_mutect,
-  seqAlignmentAnalyses_sanger,
   seqExpAnalyses,
-  seqExpAnalysesWithSpecimens,
   seqExpAnalysesWithSpecimens_incremental,
-  seqExpAnalysesWithSpecimens_incremental_after_unpublish,
   seqExpAnalyses_incremental,
-  seqExpAnalyses_incremental_after_unpublish,
-  variantCallingAnalyses,
 } from "rdpc/test/fixtures/integrationTest/mockAnalyses";
 import esb from "elastic-builder";
 import { EsHit } from "indexClinicalData/types";
@@ -44,9 +36,16 @@ import donorIndexMapping from "elasticsearch/donorIndexMapping.json";
 import { generateIndexName } from "./util";
 import { getIndexSettings, getLatestIndexName } from "elasticsearch";
 import { EgoAccessToken, EgoJwtManager } from "auth";
-import fetchAnalysesWithSpecimens from "rdpc/query/fetchAnalysesWithSpecimens";
-import fetchVariantCallingAnalyses from "rdpc/query/fetchVariantCallingAnalyses";
 import { Action } from "rdpc/query/types";
+import {
+  mockAnalysesWithSpecimensFetcher,
+  mockAnalysesWithSpecimensFetcher_incremental,
+  mockAnalysesWithSpecimensFetcher_incremental_after_unpublish,
+  mockAnalysisFetcher,
+  mockAnalysisFetcher_incremental,
+  mockAnalysisFetcher_incremental_after_unpublish,
+  mockVariantCallingFetcher,
+} from "./MockFetch";
 
 const TEST_US = "TEST-US";
 const TEST_CA = "TEST-CA";
@@ -95,155 +94,6 @@ describe("kafka integration", () => {
         groups: "",
       };
     },
-  };
-
-  const mockVariantCallingFetcher: typeof fetchVariantCallingAnalyses = ({
-    studyId,
-    rdpcUrl,
-    from,
-    size,
-    egoJwtManager,
-    donorId,
-  }): Promise<Analysis[]> => {
-    const matchesDonorId = (donor: any) =>
-      donorId ? donor.donorId === donorId : true;
-    return Promise.resolve(
-      variantCallingAnalyses
-        .filter((analysis) => analysis.donors.some(matchesDonorId))
-        .slice(from, from + size)
-    );
-  };
-
-  const mockAnalysesWithSpecimensFetcher: typeof fetchAnalysesWithSpecimens = async ({
-    studyId,
-    rdpcUrl,
-    from,
-    size,
-    egoJwtManager,
-    donorId,
-  }): Promise<Analysis[]> => {
-    const matchesDonorId = (donor: any) =>
-      donorId ? donor.donorId === donorId : true;
-    return Promise.resolve(
-      seqExpAnalysesWithSpecimens
-        .filter((analysis) => analysis.donors.some(matchesDonorId))
-        .slice(from, from + size)
-    );
-  };
-
-  const mockAnalysesWithSpecimensFetcher_incremental: typeof fetchAnalysesWithSpecimens = async ({
-    studyId,
-    rdpcUrl,
-    from,
-    size,
-    egoJwtManager,
-    donorId,
-  }): Promise<Analysis[]> => {
-    const matchesDonorId = (donor: any) =>
-      donorId ? donor.donorId === donorId : true;
-    return Promise.resolve(
-      seqExpAnalysesWithSpecimens_incremental
-        .filter((analysis) => analysis.donors.some(matchesDonorId))
-        .slice(from, from + size)
-    );
-  };
-
-  const mockAnalysesWithSpecimensFetcher_incremental_after_unpublish: typeof fetchAnalysesWithSpecimens = async ({
-    studyId,
-    rdpcUrl,
-    from,
-    size,
-    egoJwtManager,
-    donorId,
-  }): Promise<Analysis[]> => {
-    const matchesDonorId = (donor: any) =>
-      donorId ? donor.donorId === donorId : true;
-    return Promise.resolve(
-      seqExpAnalysesWithSpecimens_incremental_after_unpublish
-        .filter((analysis) => analysis.donors.some(matchesDonorId))
-        .slice(from, from + size)
-    );
-  };
-
-  const mockAnalysisFetcher: typeof fetchAnalyses = async ({
-    studyId,
-    rdpcUrl,
-    analysisType,
-    isMutect,
-    from,
-    size,
-    egoJwtManager,
-    donorId,
-  }): Promise<Analysis[]> => {
-    const matchesDonorId = (donor: any) =>
-      donorId ? donor.donorId === donorId : true;
-    return Promise.resolve(
-      analysisType === AnalysisType.SEQ_EXPERIMENT
-        ? seqExpAnalyses
-            .filter((analysis) => analysis.donors.some(matchesDonorId))
-            .slice(from, from + size)
-        : isMutect
-        ? seqAlignmentAnalyses_mutect
-            .filter((analysis) => analysis.donors.some(matchesDonorId))
-            .slice(from, from + size)
-        : seqAlignmentAnalyses_sanger
-            .filter((analysis) => analysis.donors.some(matchesDonorId))
-            .slice(from, from + size)
-    );
-  };
-
-  const mockAnalysisFetcher_incremental: typeof fetchAnalyses = async ({
-    studyId,
-    rdpcUrl,
-    analysisType,
-    isMutect,
-    from,
-    size,
-    egoJwtManager,
-    donorId,
-  }): Promise<Analysis[]> => {
-    const matchesDonorId = (donor: any) =>
-      donorId ? donor.donorId === donorId : true;
-    return Promise.resolve(
-      analysisType === AnalysisType.SEQ_EXPERIMENT
-        ? seqExpAnalyses_incremental
-            .filter((analysis) => analysis.donors.some(matchesDonorId))
-            .slice(from, from + size)
-        : isMutect
-        ? seqAlignmentAnalyses_mutect
-            .filter((analysis) => analysis.donors.some(matchesDonorId))
-            .slice(from, from + size)
-        : seqAlignmentAnalyses_sanger
-            .filter((analysis) => analysis.donors.some(matchesDonorId))
-            .slice(from, from + size)
-    );
-  };
-
-  const mockAnalysisFetcher_incremental_after_unpublish: typeof fetchAnalyses = async ({
-    studyId,
-    rdpcUrl,
-    analysisType,
-    isMutect,
-    from,
-    size,
-    egoJwtManager,
-    donorId,
-  }): Promise<Analysis[]> => {
-    const matchesDonorId = (donor: any) =>
-      donorId ? donor.donorId === donorId : true;
-    return Promise.resolve(
-      analysisType === AnalysisType.SEQ_EXPERIMENT
-        ? seqExpAnalyses_incremental_after_unpublish
-            .filter((analysis) => analysis.donors.some(matchesDonorId))
-            .slice(from, from + size)
-        : isMutect
-        ? seqAlignmentAnalyses_mutect
-            .filter((analysis) => analysis.donors.some(matchesDonorId))
-            .slice(from, from + size)
-        : seqAlignmentAnalyses_sanger
-            .filter((analysis) => analysis.donors.some(matchesDonorId))
-            .slice(from, from + size)
-    );
   };
 
   before(async () => {
@@ -1280,7 +1130,6 @@ describe("kafka integration", () => {
                 .mutectFailed
             : 0,
         ]);
-
         expect([
           hit._source.donorId,
           "sangerVcsFirstPublishedDate",
@@ -1291,7 +1140,6 @@ describe("kafka integration", () => {
           expectedRDPCData_incremental_after_unpublish[hit._source.donorId]
             .sangerVcsFirstPublishedDate,
         ]);
-
         expect([
           hit._source.donorId,
           "mutectFirstPublishedDate",
