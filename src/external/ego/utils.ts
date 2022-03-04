eb0833aac39002b7b91fd705c8e1c6d79cd2d283
joneubank <joneubank@gmail.com>
2022-03-04 18:54:50 -0500

    Remove commented code

--- src/external/ego/utils.ts
+++ src/external/ego/utils.ts
@@ -141,39 +141,3 @@ const isEgoCredential = (obj: {
     typeof obj["egoClientSecret"] === "string"
   );
 };
-
-// export default async () => {
-//   const egoPubliKey: string = await getPublicKey();
-//   return createEgoUtils(egoPubliKey);
-// };
-
-// const getPublicKey = async (): Promise<string> => {
-//   logger.info("fetching ego public key...");
-//   try {
-//     const url = urljoin(EGO_URL, "api/oauth/token/public_key");
-
-//     const response = await fetch(url, {
-//       method: "GET",
-//       headers: {
-//         "Content-type": "application/json",
-//       },
-//     });
-
-//     const key = await response.text();
-//     if (
-//       response.status != 200 ||
-//       key.indexOf("-----BEGIN PUBLIC KEY-----") === -1
-//     ) {
-//       throw new Error(`failed to fetch valid JwtPublicKey, response:  ${key}`);
-//     }
-//     const correctFormatKey = `-----BEGIN PUBLIC KEY-----\n${key
-//       .replace("-----BEGIN PUBLIC KEY-----", "")
-//       .replace("-----END PUBLIC KEY-----", "")
-//       .trim()}\n-----END PUBLIC KEY-----`;
-
-//     return correctFormatKey;
-//   } catch (err) {
-//     logger.error(`Failed to fetch ego public key ${err}`);
-//     throw new Error(err);
-//   }
-// };
