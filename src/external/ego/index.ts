e69df4ad4ea73f7c9cff36764494ecd443460078
joneubank <joneubank@gmail.com>
2022-03-29 18:52:40 -0400

    Ego client type safety

--- src/external/ego/index.ts
+++ src/external/ego/index.ts
@@ -4,21 +4,28 @@ import {
   getEgoAppCredentials,
 } from "external/ego/utils";
 import { EGO_URL_DCC, EGO_URL_RDPC } from "config";
+import logger from "logger";
 
 /**
  * Keep instance of auth clients for fetching RDPC and DCC credentials
  */
-let clients: { rdpc: AuthClient; dcc: AuthClient };
+let clients: { rdpc?: AuthClient; dcc?: AuthClient } = {};
 
 const egoHosts = { dcc: EGO_URL_DCC, rdpc: EGO_URL_RDPC };
 
 export const getEgoToken = async (cluster: "dcc" | "rdpc"): Promise<string> => {
   if (!clients[cluster]) {
+    logger.debug(`Creating Ego client for ${cluster}`);
     const egoCredentials = await getEgoAppCredentials(cluster);
     clients[cluster] = await createAuthClient(
       egoHosts[cluster],
       egoCredentials
     );
   }
-  return clients[cluster].getAuth();
+
+  if (clients[cluster]) {
+    return (clients[cluster] as AuthClient).getAuth();
+  } else {
+    throw new Error(`Unable to establish Ego Client for ${cluster}`);
+  }
 };
