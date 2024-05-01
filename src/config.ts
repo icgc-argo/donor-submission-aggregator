22050cf8375a4b4e9673a99830571d3d983a7500
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-05-01 16:43:56 -0400

    pr feedback

--- src/config.ts
+++ src/config.ts
@@ -41,9 +41,7 @@ export const EGO_APP_CLIENT_ID =
 	process.env.EGO_APP_RDPC_CLIENT_ID || 'donor-submission-aggregator';
 export const EGO_APP_CLIENT_SECRET = process.env.EGO_APP_RDPC_CLIENT_SECRET || 'top-secret';
 
-export const EGO_PUBLIC_KEY =
-	process.env.EGO_PUBLIC_KEY ||
-	`-----BEGIN PUBLIC KEY-----\r\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0lOqMuPLCVusc6szklNXQL1FHhSkEgR7An+8BllBqTsRHM4bRYosseGFCbYPn8r8FsWuMDtxp0CwTyMQR2PCbJ740DdpbE1KC6jAfZxqcBete7gP0tooJtbvnA6X4vNpG4ukhtUoN9DzNOO0eqMU0Rgyy5HjERdYEWkwTNB30i9I+nHFOSj4MGLBSxNlnuo3keeomCRgtimCx+L/K3HNo0QHTG1J7RzLVAchfQT0lu3pUJ8kB+UM6/6NG+fVyysJyRZ9gadsr4gvHHckw8oUBp2tHvqBEkEdY+rt1Mf5jppt7JUV7HAPLB/qR5jhALY2FX/8MN+lPLmb/nLQQichVQIDAQAB\r\n-----END PUBLIC KEY-----`;
+export const EGO_PUBLIC_KEY = process.env.EGO_PUBLIC_KEY || '';
 
 export const RDPC_URL = process.env.RDPC_URL || 'http://localhost:8000';
 
