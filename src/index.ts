84eb4d9456caa05ce940e2863d3c92e1d662add8
Ciarán Schütte <ciaranschutte@gmail.com>
2024-05-07 22:57:05 -0400

    Feat/#302 endpoint (#308)
    * apollo setup. mirror argo-clinical (#303)
    
    * apollo setup. mirror argo-clinical
    
    * skip TS lib check on node modules
    
    * fix path
    
    * fix typo
    
    ---------
    
    Co-authored-by: Ciaran Schutte <ciaranschutte@oicr.on.ca>
    
    * Gql schemas (#304)
    
    * apollo setup. mirror argo-clinical
    
    * fix path
    
    * add program donor summary
    
    * add missing utils and configs. in line with recent argo-clinical commits
    
    * add program donor published analysis by date
    
    * adds missing configs and utils
    
    * fix schema merging and types
    
    ---------
    
    Co-authored-by: Ciaran Schutte <ciaranschutte@oicr.on.ca>
    
    * auth function code refactor and reusable (#305)
    
    * rearrange some auth funcs
    
    * clean up imports
    
    * use ego package utils
    
    * add rdpc code from env var
    
    * remove rdpc_code property from base query args
    
    ---------
    
    Co-authored-by: Ciaran Schutte <ciaranschutte@oicr.on.ca>
    
    * fix imports
    
    * public key and prettier
    
    * use ego token utils
    
    * apollo setup. mirror argo-clinical (#303)
    
    * apollo setup. mirror argo-clinical
    
    * skip TS lib check on node modules
    
    * fix path
    
    * fix typo
    
    ---------
    
    Co-authored-by: Ciaran Schutte <ciaranschutte@oicr.on.ca>
    
    * Gql schemas (#304)
    
    * apollo setup. mirror argo-clinical
    
    * fix path
    
    * add program donor summary
    
    * add missing utils and configs. in line with recent argo-clinical commits
    
    * add program donor published analysis by date
    
    * adds missing configs and utils
    
    * fix schema merging and types
    
    ---------
    
    Co-authored-by: Ciaran Schutte <ciaranschutte@oicr.on.ca>
    
    * add route
    
    * add env feature flag
    
    * add json web token, get token from req, check auth
    
    * export get public key
    
    * add prettier org plugin
    
    * fix build issues. update prettuer packages
    
    * jwt env var unused
    
    * export unused getpublickey
    
    * npm i @apollo/server lib
    
    * use develop branch package-lock and use npm i for missing libs
    
    ---------
    
    Co-authored-by: Ciaran Schutte <ciaranschutte@oicr.on.ca>

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import applyStatusReport from "./statusReport";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { createApolloServer, gqlContext } from "gql/server";
 import { getEsClient } from "external/elasticsearch";
+import indexingRouter from "routes/indexing";
 
 (async () => {
 	/**
@@ -32,42 +33,15 @@ import { getEsClient } from "external/elasticsearch";
   });
   logger.info(`GQL server ready at port: ${GRAPHQL_PORT}`);
 
-  expressApp.post("/index/program/:program_id", async (req, res) => {
-    const programId = req.params.program_id;
-    try {
-      logger.info(
-        `received request to index program ${programId}, validating program id...`
-      );
-      // validate programId:
-      const regex = new RegExp(
-        "^[A-Z0-9][-_A-Z0-9]{2,7}[-](([A-Z][A-Z])|(INTL))$"
-      );
-      const found = programId.match(regex);
+  if (featureFlags.endpoint.index) {
+    expressApp.post("/index", indexingRouter);
+  }
 
-			if (!found) {
-				return res
-					.status(400)
-					.send(`ProgramId (${programId}) is invalid, please enter a valid programId.`);
-			} else {
-				await queueProgramUpdateEvent({
-					programId: programId,
-					type: KnownEventType.SYNC,
-					rdpcGatewayUrls: [RDPC_URL],
-				});
-				logger.info(`Program ${programId} has been queued for indexing.`);
-				return res.status(200).send(`Program ${programId} has been queued for indexing.`);
-			}
-		} catch (error) {
-			logger.error('Error in processing index program request: ' + error);
-			return res.status(500).send(`Failed to queue program ${programId} for indexing.`);
-		}
-	});
-
-	// Initialize Kafka Consumers and Producers
-	//  - Can be disabled to simplify running in dev, set FEATURE_DEV_DISABLE_KAFKA=true in .env
-	if (featureFlags.kafka) {
-		await kafka.setup();
-	}
+  // Initialize Kafka Consumers and Producers
+  //  - Can be disabled to simplify running in dev, set FLAG_DEV_DISABLE_KAFKA=true in .env
+  if (featureFlags.kafka) {
+    await kafka.setup();
+  }
 
 	logger.info('pipeline is ready!');
 	expressApp.listen(PORT, () => {
